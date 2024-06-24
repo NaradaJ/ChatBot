1ffc9305778d55864c1c98b5153a6ca7a2eb4df0
Narada Kasun <140716249+NaradaJ@users.noreply.github.com>
2024-06-24 17:53:59 +0530

    Java ChatBot for Hyperskill

---
+++ src/ChatBot.java
@@ -0,0 +1,14 @@
+//package bot;
+
+public class ChatBot {
+
+    public static void main(String[] args) {
+    // Define the bot's name and the year it was created
+    String botName = "Aid";
+    int birthYear = 2024;
+    
+    //Print Greeting msgs
+    System.out.println("Hello! My name is "+ botName + ".");
+    System.out.println("I was created in "+ birthYear + ".");
+    }
+}
